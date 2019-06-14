5679e48bae11600a7179b44614831f57231cef89
Gabriel S <gabrielssc.ti@gmail.com>
2019-06-13 23:00:18 -0300

    Test

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 function love.load()
     image = love.graphics.newImage('assets/didudxs_paradas1.png')
+    test = 'test';
     
 end
 
 function love.draw()
-    
     love.graphics.draw(image);
 end
