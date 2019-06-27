fb9126bc03761f89898951a0f189a14a6f646ec7
Eduarda Chagas <adraudech@gmail.com>
2019-06-26 23:25:08 -0300

    alterações do menu --para serem corrigidos posteriormente

--- main.lua
+++ main.lua
@@ -11,11 +11,30 @@ function loadTiles()
 
     return tiles;
 end
-
-
-    
-
 function love.load()
+--[[
+--menu and mouse
+		play={}
+	quit={}
+	for i=1,2 do
+		play[i]=love.graphics.newImage('assets/play'..i..'.png')
+	end
+	for i=1,2 do
+		quit[i]=love.graphics.newImage('assets/quit'..i..'.png')
+	end
+	actuabuttonp=play[1]
+	actualbuttonq=quit[1]
+	rato=love.graphics.newImage('assets/rato.png')
+	love.mouse.setVisible(false)
+	img={}
+	for i=1,5 do
+		img[i]=love.graphics.newImage('assets/didudxs_paradas'..i..'.png')
+	end 
+	timer = 0
+	num = 1
+	growing=true
+	actualAnimation = img[num]
+--]]
     player = {
         xPos = 100,
         yPos = 100,
@@ -62,6 +81,15 @@ function draw_map()
  end
 
 function love.draw()
+--[[
+--menu's buttons
+	love.graphics.draw(play[1], 320, 165)
+    love.graphics.draw(quit[1], 320,300)
+    love.graphics.setBackgroundColor(255,255,255)
+    love.graphics.draw(rato,love.mouse.getX(),love.mouse.getY())
+    love.graphics.draw(actualAnimation,80,500)
+    love.graphics.setBackgroundColor(255,255,255)
+--]]	
 	love.graphics.draw(play[1], 320, 165)
     love.graphics.draw(quit[1], 320,300)
     love.graphics.setBackgroundColor(255,255,255)
@@ -70,6 +98,24 @@ function love.draw()
     love.graphics.setBackgroundColor(255,255,255)
 end
 function love.update(dt)
+--[[
+-- menu's dog and bird
+	timer = timer + dt
+	if timer>=0.07 then
+		if growing==true then
+			num = num+1
+		else
+			num=num-1
+		end
+		if num==5 then
+			growing=false
+		elseif num==1 then
+			growing=true
+		end
+		actualAnimation = img[num]
+		timer = 0
+	end
+--]]
     if(isGamePaused(actGameState)) then
         --CODIGO DO MENU
     else
